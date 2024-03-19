97f0078fd276628dd76337aca4d966b797a7788c
Moboss2k05 <mobarizmirza.rizwan@gmail.com>
2024-03-19 14:13:26 +0000

    added a comment

--- MyPokemonBlazorApp/Context/DatabaseContext.cs
+++ MyPokemonBlazorApp/Context/DatabaseContext.cs
@@ -12,7 +12,7 @@ namespace MyPokemonBlazorApp.Context
         {
             var folder = Environment.SpecialFolder.MyDocuments;
             var path = Environment.GetFolderPath(folder);
-            var dbpath = Path.Join(path, "pokemon.db");
+            var dbpath = Path.Join(path, "pokemon.db"); //this is my database
             optionsBuilder.UseSqlite($"Data Source={dbpath}");
         }
     }
