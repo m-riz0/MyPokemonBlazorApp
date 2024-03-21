abd119bd3ad01ad492225864586f536348f08bbd
Moboss2k05 <mobarizmirza.rizwan@gmail.com>
2024-03-21 09:31:30 +0000

    fully working images and info page

--- MyPokemonBlazorApp/Program.cs
+++ MyPokemonBlazorApp/Program.cs
@@ -1,4 +1,5 @@
 using MyPokemonBlazorApp.Components;
+using MyPokemonBlazorApp.Components.Shared;
 using MyPokemonBlazorApp.Context;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddScoped<PokemonImageGrabber>();
 
 builder.Services.AddDbContext<DatabaseContext>();
 
