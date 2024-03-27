15f806c605a10538c75c6ef707388ce41e1938bf
Moboss2k05 <mobarizmirza.rizwan@gmail.com>
2024-03-27 10:36:01 +0000

    trainers page working

--- MyPokemonBlazorApp/Model/Trainer.cs
+++ MyPokemonBlazorApp/Model/Trainer.cs
@@ -1,9 +1,14 @@
-﻿namespace MyPokemonBlazorApp.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyPokemonBlazorApp.Model
 {
     public class Trainer
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
         public Pokemon Companion { get; set; }
     }
 }
